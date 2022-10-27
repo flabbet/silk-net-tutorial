91a3e347a5f64c1a83b3131ba1d8655f6c2427d0
Krzysztof Krysiński <flubbet@gmail.com>
2022-10-27 08:41:11 +0200

    Camera and gif

---
+++ SilkNet/Gif.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+using Silk.NET.OpenGL;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SilkNet;
+
+public class Gif
+{
+    public int FrameCount => _frames.Length;
+    
+    private Image<Rgba32> _gifImage;
+    private Texture[] _frames;
+    private GL _gl;
+    
+    public Gif(GL gl, string path)
+    {
+        _gifImage = Image.Load<Rgba32>(path);
+        _gl = gl;
+        LoadFrames();
+    }
+
+    public Texture GetFrame(int frame)
+    {
+        return _frames[frame];
+    }
+    
+    private void LoadFrames()
+    {
+        _frames = new Texture[_gifImage.Frames.Count];
+        for (int i = 0; i < _gifImage.Frames.Count; i++)
+        {
+            _frames[i] = new Texture(_gl, GetFrameData(i), (uint)_gifImage.Width, (uint)_gifImage.Height);
+        }
+    }
+    
+    private Span<byte> GetFrameData(int frame)
+    {
+        var memoryGroup = _gifImage.Frames[frame].PixelBuffer.MemoryGroup;
+        return MemoryMarshal.AsBytes(memoryGroup[0].Span);
+    }
+}
--- SilkNet/Program.cs
+++ SilkNet/Program.cs
@@ -20,13 +20,25 @@ public class Program
     private static VertexArrayObject<float, uint> _vao;
     private static Shader _shaderProgram;
     private static Texture _texture;
-    private static Transform[] _transforms = new Transform[4];
+    private static Gif _dogGif;
 
     private static Vector3 _cameraPosition = new Vector3(0, 1, 3f);
+    private static Vector3 _cameraFront = new Vector3(0, 0, -1);
     private static Vector3 _cameraTarget = Vector3.Zero;
     private static Vector3 _cameraDirection = Vector3.Normalize(_cameraPosition - _cameraTarget);
     private static Vector3 _cameraRight = Vector3.Normalize(Vector3.Cross(Vector3.UnitY, _cameraDirection));
     private static Vector3 _cameraUp = Vector3.Cross(_cameraDirection, _cameraRight);
+
+    private static float _cameraYaw = -90f;
+    private static float _cameraPitch = 0f;
+    private static float _cameraZoom = 45f;
+
+    private static Vector2 _lastMousePosition;
+    private static IKeyboard _primaryKeyboard;
+    
+    private const int GifSpeed = 1;
+    private static float _normalizedTime;
+    private static int _currentFrame;
     
     private static void Main(string[] args)
     {
@@ -48,6 +60,7 @@ public class Program
     {
         IInputContext input = _window.CreateInput();
         RegisterKeyboards(input);
+        RegisterMouse(input);
 
         vertexShader = ShaderLoader.LoadRaw("VertexShader");
         fragmentShader = ShaderLoader.LoadRaw("FragmentShader");
@@ -63,27 +76,54 @@ public class Program
         _vao.VertexAttributePointer(1, 2, VertexAttribPointerType.Float, 5, 3);
 
         _shaderProgram = new Shader(_gl, vertexShader, fragmentShader);
-        _texture = new Texture(_gl, "texture.png");
+        _texture = new Texture(_gl, "Images/texture.png");
+        _dogGif = new Gif(_gl, "Images/dancing-dog.gif");
     }
     
-    private static void OnUpdate(double value)
+    private static void OnUpdate(double deltaTime)
     {
-       
+        float moveSpeed = 2.5f * (float)deltaTime;
+
+        if (_primaryKeyboard.IsKeyPressed(Key.W))
+        {
+            _cameraPosition += moveSpeed * _cameraFront;
+        }
+        if (_primaryKeyboard.IsKeyPressed(Key.S))
+        {
+            _cameraPosition -= moveSpeed * _cameraFront;
+        }
+        if (_primaryKeyboard.IsKeyPressed(Key.A))
+        {
+            _cameraPosition -= Vector3.Normalize(Vector3.Cross(_cameraFront, _cameraUp)) * moveSpeed;
+        }
+        if (_primaryKeyboard.IsKeyPressed(Key.D))
+        {
+            _cameraPosition += Vector3.Normalize(Vector3.Cross(_cameraFront, _cameraUp)) * moveSpeed;
+        }
+
+        _normalizedTime = Math.Clamp(_normalizedTime + (float)deltaTime, 0, 1);
+        if(_normalizedTime >= 1)
+        {
+            _normalizedTime = 0;
+        }
+        
+        _currentFrame = (int)(_normalizedTime * _dogGif.FrameCount);
     }
     
-    private static unsafe void OnRender(double deltaTime)
+    private static void OnRender(double deltaTime)
     {
         _gl.Enable(EnableCap.DepthTest);
         _gl.Clear((uint) (ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit));
         _vao.Bind();
         
         _shaderProgram.Use();
-        _texture.Bind();
+        _dogGif.GetFrame(_currentFrame).Bind();
         
         var difference = (float)(_window.Time * 100);
-        var model = Matrix4x4.CreateRotationY(MathHelper.DegreesToRadians(difference));
-        var view = Matrix4x4.CreateLookAt(_cameraPosition, _cameraTarget, _cameraUp);
-        var projection = Matrix4x4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), _window.Size.X / _window.Size.Y, 0.1f, 100f);
+        float differenceRadians = MathHelper.DegreesToRadians(difference);
+        var model = Matrix4x4.CreateRotationY(differenceRadians) * Matrix4x4.CreateRotationX(differenceRadians);
+        var view = Matrix4x4.CreateLookAt(_cameraPosition, _cameraPosition + _cameraFront, _cameraUp);
+        var projection = Matrix4x4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(_cameraZoom), _window.Size.X / _window.Size.Y, 0.1f, 100f);
         
         _shaderProgram.SetUniform("uModel", model);
         _shaderProgram.SetUniform("uView", view);
@@ -103,9 +143,65 @@ public class Program
 
     private static void RegisterKeyboards(IInputContext input)
     {
+        _primaryKeyboard = input.Keyboards.FirstOrDefault();
+        
         for (int i = 0; i < input.Keyboards.Count; i++)
         {
             input.Keyboards[i].KeyDown += OnKeyDown;
+        }
+    }
+
+    private static void RegisterMouse(IInputContext input)
+    {
+        for (int i = 0; i < input.Mice.Count; i++)
+        {
+            var mouse = input.Mice[i];
+            mouse.Cursor.CursorMode = CursorMode.Raw;
+            mouse.MouseMove += OnMouseMove;
+            mouse.Scroll += OnScroll;
+            mouse.Click += OnMouseClick;
+        }
+    }
+
+    private static void OnMouseClick(IMouse mouse, MouseButton clickBtn, Vector2 pos)
+    {
+        if (clickBtn == MouseButton.Left)
+        {
+            _lastMousePosition = pos;
+        }
+    }
+
+    private static void OnScroll(IMouse mouse, ScrollWheel wheel)
+    {
+        _cameraZoom = Math.Clamp(_cameraZoom - wheel.Y, 1f, 45f);
+    }
+
+    private static void OnMouseMove(IMouse mouse, Vector2 pos)
+    {
+        if(!mouse.IsButtonPressed(MouseButton.Left)) return;
+        
+        float lookSensitivity = 0.1f;
+        if (_lastMousePosition == default)
+        {
+            _lastMousePosition = pos;
+        }
+        else
+        {
+            float xOffset = (pos.X - _lastMousePosition.X) * lookSensitivity;
+            float yOffset = (pos.Y - _lastMousePosition.Y) * lookSensitivity;
+            _lastMousePosition = pos;
+            
+            _cameraYaw += xOffset;
+            _cameraPitch -= yOffset;
+
+            _cameraPitch = Math.Clamp(_cameraPitch, -89f, 89f);
+
+            _cameraDirection.X = MathF.Cos(MathHelper.DegreesToRadians(_cameraYaw) *
+                                           MathF.Cos(MathHelper.DegreesToRadians(_cameraPitch)));
+            _cameraDirection.Y = MathF.Sin(MathHelper.DegreesToRadians(_cameraPitch));
+            _cameraDirection.Z = MathF.Sin(MathHelper.DegreesToRadians(_cameraYaw)) *
+                                 MathF.Cos(MathHelper.DegreesToRadians(_cameraPitch));
+            _cameraFront = Vector3.Normalize(_cameraDirection);
         }
     }
 
