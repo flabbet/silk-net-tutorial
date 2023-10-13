7ffaa9ffc8cef8a51e825e82d0a63b844a714572
Krzysztof Krysiński <flubbet@gmail.com>
2023-10-13 13:21:31 +0200

    Create QueueFamilyIndices.cs

---
+++ SilkNet.Vulkan/QueueFamilyIndices.cs
@@ -0,0 +1,10 @@
+﻿namespace SilkNet.Vulkan;
+
+struct QueueFamilyIndices
+{
+    public uint? GraphicsFamily { get; set; }
+    public bool IsComplete()
+    {
+        return GraphicsFamily.HasValue;
+    }
+}
